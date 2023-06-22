5c165a0bede4a2ab16f78802a038829c7510abd4
amber-ahmed <aammber321@gmail.com>
2023-06-22 23:55:18 +0530

    deployed

--- server/app.js
+++ server/app.js
@@ -6,6 +6,13 @@ import {Server} from 'socket.io'
 import http from 'http'
 import socket from './socket.js'
 dotenv.config()
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const app = express()
 const server = http.createServer(app)
 const io = new Server(server,{
@@ -18,12 +25,16 @@ const io = new Server(server,{
 
 })
 app.use(express.json())
-app.get('/',(req,res)=>{
-    res.send('from server')
-})
+// app.get('/',(req,res)=>{
+//     res.send('from server')
+// })
 app.use('/api/user',router)
 socket(io)
 
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 server.listen(process.env.PORT,()=>{
     console.log('server started at',  process.env.PORT)
 })
